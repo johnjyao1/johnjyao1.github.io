ec6d7327c1679c2208c27f9c424ca044192f41c4
John Yao <john.j.yao@gmail.com>
2024-10-27 00:12:10 -0400

    Updated CSS.  Automatically loads most recent week's words. Automatically checks word on change.

--- spanish/spanish.js
+++ spanish/spanish.js
@@ -8,37 +8,44 @@ function checkAnswer(word, answer_form) {
     }
 }
 
+function setSpellingWords(root, words) {
+    root.empty();
+
+    const week = $('#selection').val();
+    if (week != "") {
+        let word_audio = [];
+        for(let i=0; i < words.length; i++) {
+            const word = words[i]
+            word_audio.push(new Audio("media/"+word+".mp3"))
+            root.append($("<tr>", {"id": "word-container-"+i}));
+            $("#word-container-"+i).append($("<td>", {"class": "enum"}).text(i+1+". "));
+            $("#word-container-"+i).append($("<td>", {"class": "word"}).text(word));
+            $("#word-container-"+i).append($("<td>")).append($("<button>", {"id": i, "class": "speak"}).text("\u{1F509}"));
+            $("#"+i).on("click", function() { 
+                word_audio[i].playbackRate = $('#rate').val()
+                word_audio[i].play();
+            });
+            $("#word-container-"+i).append($("<td>")).append($("<input>", {"id": "answer-" + i, "type": "text"}))
+            $("#answer-"+i).on("change", function(event) {
+                checkAnswer(word, $('#answer-'+i));
+            })
+        }
+        $("#show-hide-words").text("Show Words");
+        $(".word").hide();
+    }
+}
+
 fetch("data.json").then((response) => response.json()).then((json) => {
-    const data = json.data
+    var data = json.data
 
     for(let i=0; i<data.length; i++) {
         $('#selection').append($("<option>", {value: i}).text(data[i].date))
     }
+    $('#selection').val(data.length-1)
+    setSpellingWords($("#container"), data[$('#selection').val()].words)
 
     $('#selection').on("change", function(event) {
-        $("#container").empty();
-        const week = $('#selection').val();
-        if (week != "") {
-            let word_audio = [];
-            for(let i=0; i < data[week].words.length; i++) {
-                const word = data[week].words[i]
-                word_audio.push(new Audio("media/"+word+".mp3"))
-                $("#container").append($("<tr>", {"id": "word-container-"+i}));
-                $("#word-container-"+i).append($("<td>", {"class": "enum"}).text(i+1+". "));
-                $("#word-container-"+i).append($("<td>", {"class": "word"}).text(word));
-                $("#word-container-"+i).append($("<td>")).append($("<button>", {"id": i, "class": "speak"}).text("\u{1F509}"));
-                $("#"+i).on("click", function() { 
-                    word_audio[i].playbackRate = $('#rate').val()
-                    word_audio[i].play();
-                });
-                $("#word-container-"+i).append($("<td>")).append($("<input>", {"id": "answer-" + i, "type": "text"}))
-                $("#word-container-"+i).append($("<td>")).append($("<button>", {"id": "check-answer-"+i}).text("Check"));
-                $("#check-answer-"+i).on("click", function(event) {
-                    checkAnswer(word, $('#answer-'+i));
-                })
-            }
-            $(".word").hide();
-        }
+        setSpellingWords($("#container"), data[$('#selection').val()].words)
     });
 });
 $("#show-hide-words").on("click", function(event) {
